81cd0296a53e31eef9c51dab7639c9248f0cfa86
l10178 <l10178@163.com>
2025-11-14 13:15:20 +0800

    build

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { ThemeContextProvider } from './theme/ThemeContext';
@@ -9,11 +9,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/ui">
       <ThemeContextProvider>
         <App />
       </ThemeContextProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
